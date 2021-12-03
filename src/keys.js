74c42583d31ccb4d51f3e4cf3602080decd4f9f4
KrissGaliano <kriss.galiano@tecup.edu.pe>
2021-12-03 09:39:25 -0500

    semana14 app node

---
+++ src/keys.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        //host: 'kriss-version.cpfncagmdjlm.us-east-1.rds.amazonaws.com',
+        //user: 'admin',
+        //password: '12345678',
+        //database: 'agenda_KG'
+
+        host:'ec2-3-87-215-185.compute-1.amazonaws.com',
+        user:'kriss',
+        password:'12345678a',
+        database: 'crud'
+    }
+
+};
